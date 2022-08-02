173ec25722c81eabd9430d03f4e917568f7e0194
Hualin Mei <mhl0116@gmail.com>
2022-08-01 23:53:36 -0700

    make basic shape comparison plots

--- python/examine_dataframe.py
+++ python/examine_dataframe.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import pprint
 
-fname = "/home/users/smay/HiggsDNA/dipho_presel_forHualin_4Jul2022/ggH_M125_2018/merged_nominal.parquet"
+#fname = "/home/users/smay/HiggsDNA/dipho_presel_forHualin_4Jul2022/ggH_M125_2018/merged_nominal.parquet"
+fname = "/home/users/smay/HiggsDNA/dipho_presel_forHualin_29Jul2022/DiPhoton_2018/merged_nominal.parquet"
 df = pd.read_parquet(fname)
 print (f"number of events passed diphoton pre-selection is: {len(df)}")
 print (f"sum of events weight passed diphoton pre-selection is: {df.weight_central.sum()}")
